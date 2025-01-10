6b0250cb1e0a194696402ac1a1b8296483d505b1
Fabio Kusaba <fabinhokb44@gmail.com>
2025-01-10 19:40:54 -0300

    create category dto

---
+++ src/main/java/br/com/fabiokusaba/blog/domain/dtos/CategoryDTO.java
@@ -0,0 +1,19 @@
+package br.com.fabiokusaba.blog.domain.dtos;
+
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryDTO {
+
+    private UUID id;
+    private String name;
+    private long postCount;
+}
