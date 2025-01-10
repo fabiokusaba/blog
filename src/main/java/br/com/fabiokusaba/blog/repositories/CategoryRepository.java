9361005fd5024fb8714a7bc36bb4e98ba951a6d2
Fabio Kusaba <fabinhokb44@gmail.com>
2025-01-10 19:48:32 -0300

    add custom query

--- src/main/java/br/com/fabiokusaba/blog/repositories/CategoryRepository.java
+++ src/main/java/br/com/fabiokusaba/blog/repositories/CategoryRepository.java
@@ -1,13 +1,18 @@
 package br.com.fabiokusaba.blog.repositories;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.fabiokusaba.blog.domain.entities.Category;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, UUID> {
+
+    @Query("SELECT c FROM Category c LEFT JOIN FETCH c.posts")
+    List<Category> findAllWithPostCount();
 
 }
