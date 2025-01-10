b34a0cc896aeb996b9cccaf27f40c9ad8db2d6dc
Fabio Kusaba <fabinhokb44@gmail.com>
2025-01-10 19:33:51 -0300

    create category repository

---
+++ src/main/java/br/com/fabiokusaba/blog/repositories/CategoryRepository.java
@@ -0,0 +1,13 @@
+package br.com.fabiokusaba.blog.repositories;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.fabiokusaba.blog.domain.entities.Category;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, UUID> {
+
+}
