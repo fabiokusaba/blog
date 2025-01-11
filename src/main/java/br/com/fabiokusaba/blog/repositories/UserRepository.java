da598564ae743a0b27df4bfdc7af2495d63c7ce2
Fabio Kusaba <fabinhokb44@gmail.com>
2025-01-10 21:25:02 -0300

    add find by email

--- src/main/java/br/com/fabiokusaba/blog/repositories/UserRepository.java
+++ src/main/java/br/com/fabiokusaba/blog/repositories/UserRepository.java
@@ -1,5 +1,6 @@
 package br.com.fabiokusaba.blog.repositories;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,5 +10,7 @@ import br.com.fabiokusaba.blog.domain.entities.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
+
+    Optional<User> findByEmail(String email);
 
 }
