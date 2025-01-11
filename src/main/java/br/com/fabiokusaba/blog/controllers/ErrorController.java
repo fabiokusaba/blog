4b48dbb3505dcdc01ecfc5115d1564f0cc3288dd
Fabio Kusaba <fabinhokb44@gmail.com>
2025-01-10 21:33:24 -0300

    add new error

--- src/main/java/br/com/fabiokusaba/blog/controllers/ErrorController.java
+++ src/main/java/br/com/fabiokusaba/blog/controllers/ErrorController.java
@@ -2,6 +2,7 @@ package br.com.fabiokusaba.blog.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,6 +44,16 @@ public class ErrorController {
             .build();
 
         return new ResponseEntity<>(error, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ApiErrorResponse> handleBadCredentialsException(BadCredentialsException ex) {
+        ApiErrorResponse error = ApiErrorResponse.builder()
+            .status(HttpStatus.UNAUTHORIZED.value())
+            .message("Incorrect username or password")
+            .build();
+
+        return new ResponseEntity<>(error, HttpStatus.UNAUTHORIZED);
     }
 
 }
