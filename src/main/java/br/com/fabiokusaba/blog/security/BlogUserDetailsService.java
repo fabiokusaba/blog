b99f1c7239f4d9fdb0d054e31af50c8288d480c3
Fabio Kusaba <fabinhokb44@gmail.com>
2025-01-10 21:30:25 -0300

    create blog user details service

---
+++ src/main/java/br/com/fabiokusaba/blog/security/BlogUserDetailsService.java
@@ -0,0 +1,24 @@
+package br.com.fabiokusaba.blog.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import br.com.fabiokusaba.blog.domain.entities.User;
+import br.com.fabiokusaba.blog.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class BlogUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+            .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+
+        return new BlogUserDetails(user);
+    }
+
+}
