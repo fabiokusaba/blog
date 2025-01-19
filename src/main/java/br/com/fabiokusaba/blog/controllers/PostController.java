77ffe099bb58f0509c111ae0948f1c428b75c5fd
Fabio Kusaba <fabinhokb44@gmail.com>
2025-01-19 18:12:18 -0300

    implement delete post endpoint

--- src/main/java/br/com/fabiokusaba/blog/controllers/PostController.java
+++ src/main/java/br/com/fabiokusaba/blog/controllers/PostController.java
@@ -76,4 +76,10 @@ public class PostController {
         PostDTO postDTO = postMapper.toPostDTO(post);
         return ResponseEntity.ok(postDTO);
     }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<Void> deletePost(@PathVariable UUID id) {
+        postService.deletePost(id);
+        return ResponseEntity.noContent().build();
+    }
 }
